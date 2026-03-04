0960685ffb2537e6dd4804cb211dce24fffcce36
kess <kesslermentele@gmail.com>
2026-03-04 09:56:35 -0800

    Began codemod functionality for crucible editing

--- enhancedfirearms/enhancedfirearmsModSystem.cs
+++ enhancedfirearms/enhancedfirearmsModSystem.cs
@@ -1,9 +1,23 @@
 ﻿using HarmonyLib;
 using Vintagestory.API.Common;
 
-namespace enhancedfirearms;
+namespace EnhancedFirearms;
 
-public class enhancedfirearmsModSystem : ModSystem
+public class EnhancedFirearmsModSystem : ModSystem
 {
     private Harmony patcher;
+
+    public override void Start(ICoreAPI api)
+    {
+        if (!Harmony.HasAnyPatches(Mod.Info.ModID))
+        {
+            patcher = new Harmony(Mod.Info.ModID);
+            patcher.PatchCategory(Mod.Info.ModID);
+        }
+    }
+
+    public override void Dispose()
+    {
+        patcher?.UnpatchAll(Mod.Info.ModID);
+    }
 }
