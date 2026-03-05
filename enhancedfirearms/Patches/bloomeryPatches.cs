daae9c446cefdad25a56ae298af254770b272cb3
kess <kesslermentele@gmail.com>
2026-03-05 12:42:23 -0800

    Made Salt Cake stack the same as peat.

---
+++ enhancedfirearms/Patches/bloomeryPatches.cs
@@ -0,0 +1,24 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using HarmonyLib;
+// using Vintagestory.API.Common.Entities;
+// using Vintagestory.API.Common;
+// using Vintagestory.GameContent;
+// using Vintagestory.API.Client;
+// using Vintagestory.API.Config;
+// using Vintagestory.API.Server;
+//
+// namespace EnhancedFirearms.Patches
+// {
+//     [HarmonyPatchCategory("enhancedfirearms.bloomery")]
+//     internal static class EnhancedFirearmsPatches
+//     {
+//         [HarmonyPostfix()]
+//         [HarmonyPatch(typeof(BlockEntityBloomery))]
+//         
+//         
+//     }
+// }
